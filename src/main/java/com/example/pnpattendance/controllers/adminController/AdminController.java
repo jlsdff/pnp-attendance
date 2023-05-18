9803f83730466c609ad38ab0dc43d0c9a64ef1d4
Julius Duff <8215455@ntc.edu.ph>
2023-05-18 21:52:09 +0800

    added crossorigin

--- src/main/java/com/example/pnpattendance/controllers/adminController/AdminController.java
+++ src/main/java/com/example/pnpattendance/controllers/adminController/AdminController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/v1/admin")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AdminController {
 
     @Autowired
--- src/main/java/com/example/pnpattendance/controllers/recordController/RecordController.java
+++ src/main/java/com/example/pnpattendance/controllers/recordController/RecordController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/record")
+@CrossOrigin(origins = "http://localhost:3000")
 public class RecordController {
 
     @Autowired
